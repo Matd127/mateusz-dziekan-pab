bc78c9a64de5baa5843f48201812bbdf3e0027ec
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-03-11 20:14:48 +0100

    kalkulator

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var app = express();
-// const num1 = 5;
-// const num2 = 3;
-// const dodaj = num1 + num2;
-// const usun = num1 - num2;
-// const podziel = num1 / num2;
-// const pomnoz = num1 - num2;
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
-app.get('/:num1/:num2', function (req, res) {
-    var operation = req.query;
-    var num1 = req.query.num1;
-    var num2 = req.query.num2;
-    res.send(num1 + num2);
+app.get('/:operacja/:num1/:num2', function (req, res) {
+    var operacja = req.params.operacja;
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    if (operacja == 'dodaj') {
+        var wynik = num1 + num2;
+        res.send('' + wynik);
+    }
+    if (operacja == 'usun') {
+        var wynik = num1 - num2;
+        res.send('' + wynik);
+    }
+    if (operacja == 'podziel') {
+        var wynik = num1 / num2;
+        res.send('' + wynik);
+    }
+    if (operacja == 'pomnoz') {
+        var wynik = num1 * num2;
+        res.send('' + wynik);
+    }
 });
 app.listen(3000);
--- index.ts
+++ index.ts
@@ -1,25 +1,36 @@
 const express = require('express')  
 const app = express()  
 
-// const num1 = 5;
-// const num2 = 3;
-// const dodaj = num1 + num2;
-// const usun = num1 - num2;
-// const podziel = num1 / num2;
-// const pomnoz = num1 - num2;
-
 app.get('/', function (req, res) {  
   res.send('Hello World')  
 })  
 
-app.get('/:num1/:num2', function(req, res){
-    const operation = req.query
-    const num1 = req.query.num1
-    const num2 = req.query.num2
-    res.send(num1 + num2)
-    
-})
+app.get('/:operacja/:num1/:num2', function(req, res){
 
+    let operacja = req.params.operacja
+    let num1 : number = parseInt(req.params.num1)
+    let num2 : number = parseInt(req.params.num2)
+
+    if(operacja == 'dodaj'){
+      let wynik : number = num1 + num2
+      res.send(''+ wynik)  
+    }
+
+    if(operacja == 'usun'){
+      let wynik : number = num1 - num2
+      res.send(''+ wynik)  
+    }
+
+    if(operacja == 'podziel'){
+      let wynik : number = num1 / num2
+      res.send(''+ wynik)  
+    }
+
+    if(operacja == 'pomnoz') {
+      let wynik : number = num1 * num2
+      res.send(''+ wynik)  
+    }
+})
 
 
 app.listen(3000)  
