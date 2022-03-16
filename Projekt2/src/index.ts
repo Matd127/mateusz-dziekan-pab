d38d8058ec5c96dcf2dc134e9a4d1759e074a039
Mateusz Dziekan <mateusz.dziekan@microsoft.wsei.edu.pl>
2022-03-16 19:50:10 +0300

    16.03.2022

---
+++ Projekt2/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import {Request, Response} from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/note', function(req: Request, res: Response){
+  const note = {
+    title: 'Notatka'
+}
+  const jsonNote = JSON.stringify(note)
+  res.status(201)
+  res.send(note)
+})
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('GET Hello World')
+})
+
+app.post('/', function (req: Request, res: Response) {
+  console.log(req.body) // e.x. req.body.title 
+  res.status(200).send('POST Hello World')
+})
+
+app.listen(3000)
